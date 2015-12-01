MAE and CategoricalCrossEntropy losses added. Several bugs in explicit gradients calculation in tests fixed.

nnets/loss.py
@@ -10,6 +10,12 @@ class MSE(AutogradLoss):
         return np.mean((real - predicted)**2)
 
 
+class MAE(AutogradLoss):
+
+    def value(self, real, predicted):
+        return np.mean(np.abs(real - predicted))
+
+
 class BinaryCrossEntropy(AutogradLoss):
 
     def __init__(self, eps=1e-15):
@@ -17,7 +23,7 @@ class BinaryCrossEntropy(AutogradLoss):
         self.eps = eps
 
     def value(self, real, predicted):
-        return np.mean(-real*np.log(predicted)-(1.0-real)*np.log(predicted))
+        return -np.mean(real*np.log(predicted)+(1.0-real)*np.log(1.0-predicted))
 
     def preprocess(self, real, predicted):
         # Since log loss is undefined in p = 0.0 and p = 1.0
@@ -25,3 +31,9 @@ class BinaryCrossEntropy(AutogradLoss):
         predicted[predicted > 1.0-self.eps] = 1.0-self.eps
 
         return real, predicted
+
+
+class CategoricalCrossEntropy(AutogradLoss):
+
+    def value(self, real, predicted):
+        return -np.mean(np.dot(real, np.log(predicted)))

nnets/test/tests_autograd.py
@@ -2,31 +2,43 @@ from nose.tools import ok_
 from nose.tools import nottest
 
 import numpy as np
-from ..loss import MSE, BinaryCrossEntropy
+from ..loss import MSE, MAE, BinaryCrossEntropy
 from ..activation import IdentityActivation, SigmoidActivation, TanhActivation, HardTanhActivation, RectifierActivation
 
 
 def test_mse():
     mse = MSE()
 
-    real = np.array([1.0, 2.0, 1.0, 3.0])
-    predicted = np.array([0.99, 1.87, 1.1, 2.7])
+    real = np.array([1.0, 2.0, 1.0, 3.0, 1.0])
+    predicted = np.array([0.99, 1.87, 1.1, 2.7, 0.95])
 
     # Real gradient for MSE, for provided real an predicted arrays
     def real_gradient(real, predicted):
-        return 0.5 * (predicted - real)
+        return 2.0 * (predicted - real) / len(real)
 
     ok_(np.allclose(mse.gradient(real, predicted), real_gradient(real, predicted)))
 
 
-def binary_cross_entropy():
-    log_loss = BinaryCrossEntropy()
+def test_mae():
+    mae = MAE()
 
-    real = np.array([0.0, 0.0, 1.0, 1.0])
-    predicted = np.array([0.05, 0.17, 0.65, 0.99])
+    real = np.array([1.0, 2.0, 1.0, 3.0])
+    predicted = np.array([0.99, 1.87, 1.1, 2.7])
 
     def real_gradient(real, predicted):
-        return (1.0-real)/(1.0-predicted) - real/predicted
+        return np.sign(predicted - real) / len(real)
+
+    ok_(np.allclose(mae.gradient(real, predicted), real_gradient(real, predicted)))
+
+
+def test_binary_cross_entropy():
+    log_loss = BinaryCrossEntropy()
+
+    real = np.array([0.0, 0.0, 1.0, 1.0, 1.0])
+    predicted = np.array([0.05, 0.17, 0.65, 0.99, 0.87])
+
+    def real_gradient(real, predicted):
+        return -(real/predicted - (1.0-real)/(1.0-predicted))/len(real)
 
     ok_(np.allclose(log_loss.gradient(real, predicted), real_gradient(real, predicted)))
 